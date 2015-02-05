7842142f91bed72598a0b3bd4bea00d291f729a3
montanaj <clairejencks@gmail.com>
2015-02-05 12:40:04 -0600

    first commit

---
+++ Algorythm2/PlaylistDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlaylistDetailViewController.h
+//  Algorythm2
+//
+//  Created by Claire on 2/5/15.
+//  Copyright (c) 2015 Jencks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlaylistDetailViewController : UIViewController
+
+@end
